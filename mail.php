0a2511aa1205f61a4a3c4a0969f401b760658fb5
Tauseef Mamun <tauseefim@yahoo.com>
2022-09-14 15:42:55 -0400

    make it better

---
+++ mail.php
@@ -0,0 +1,12 @@
+<?php
+// the message
+
+ $userAnswer = $_POST['name'];
+$msg = "First line of text\nSecond line of text";
+
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("someone@example.com","My subject",$userAnswer);
+?>
