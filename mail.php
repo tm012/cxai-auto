c89a730fd8589f3f8f43c5f8cebde290d5c1c1eb
Tauseef Mamun <tauseefim@yahoo.com>
2022-09-14 15:53:49 -0400

    make it better

--- mail.php
+++ mail.php
@@ -6,7 +6,13 @@ $msg = "First line of text\nSecond line of text";
 
 // use wordwrap() if lines are longer than 70 characters
 $msg = wordwrap($msg,70);
-
+echo $userAnswer ;
 // send email
-mail("tauseefmamun@gmail.com","My subject",wordwrap($userAnswer));
+try {
+	mail("tauseefmamun@gmail.com","My subject",wordwrap($userAnswer));
+	echo 'If you see this, the number is 1 or below';
+}
+catch(Exception $e) {
+  echo 'Message: ' .$e->getMessage();
+}
 ?>
