3571b9f3bf887621277730cce9ec35e79fc3622f
Tauseef Mamun <tauseefim@yahoo.com>
2022-09-14 16:04:01 -0400

    make it better

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 <?php
 // the message
 echo "hello world";
- $userAnswer = $_GET['name'];
+ $userAnswer = $_POST['name'];
 $msg = "First line of text\nSecond line of text";
 
 // use wordwrap() if lines are longer than 70 characters
