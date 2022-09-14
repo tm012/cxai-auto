5a40008927e1b195baeaa949f860b4fc350b0583
Tauseef Mamun <tauseefim@yahoo.com>
2022-09-14 15:46:52 -0400

    make it better

--- mail.php
+++ mail.php
@@ -8,5 +8,5 @@ $msg = "First line of text\nSecond line of text";
 $msg = wordwrap($msg,70);
 
 // send email
-mail("tmamun@mtu.edu","My subject",$userAnswer);
+mail("tauseefmamun@gmail.com","My subject",$userAnswer);
 ?>
